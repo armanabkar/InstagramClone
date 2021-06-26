ccf890c5a994990de144bdbb31f6fbfa21943aa6
Arman Abkar <armanabkar@gmail.com>
2021-06-26 12:12:06 +0430

    update profile page

--- InstagramClone/Controllers/SettingsViewController.swift
+++ InstagramClone/Controllers/SettingsViewController.swift
@@ -11,12 +11,18 @@ import Firebase
 class SettingsViewController: UIViewController {
     
     @IBOutlet weak var userLabel: UILabel!
+    @IBOutlet weak var userPostsCount: UILabel!
+
+    var posts: [Post] = []
+    var currentUser: User?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let currentUser = Auth.auth().currentUser
+        currentUser = Auth.auth().currentUser
         userLabel.text = currentUser?.email
+        
+        getUserData()
     }
     
     @IBAction func logoutClicked(_ sender: Any) {
@@ -25,6 +31,37 @@ class SettingsViewController: UIViewController {
             self.performSegue(withIdentifier: K.authSegue, sender: nil)
         } catch let signOutError as NSError {
             UIAlertController.showAlert(message: signOutError.localizedDescription, from: self)
+        }
+    }
+    
+    func getUserData() {
+        let fireStoreDatabase = Firestore.firestore()
+        fireStoreDatabase.collection(K.Firestore.collectionName).order(by: K.Firestore.dateField, descending: true).addSnapshotListener { (snapshot, error) in
+            if error != nil {
+                UIAlertController.showAlert(message: error?.localizedDescription ?? K.error.title, from: self)
+            } else {
+                if snapshot?.isEmpty != true && snapshot != nil {
+                    self.posts.removeAll(keepingCapacity: false)
+                    
+                    for document in snapshot!.documents {
+                        let documentID = document.documentID
+                        
+                        if let postedBy = document.get(K.Firestore.postedByField) as? String,
+                           let postComment = document.get(K.Firestore.postCommentField) as? String,
+                           let likes = document.get(K.Firestore.likesField) as? Int,
+                           let imageUrl = document.get(K.Firestore.imageUrlField) as? String {
+                            let newPost = Post(postedBy: postedBy, postComment: postComment, likes: likes, imageUrl: imageUrl, documentID: documentID)
+                            self.posts.append(newPost)
+                        }
+                        
+                        DispatchQueue.main.async {
+                            let currentUserPosts = self.posts.filter { $0.postedBy ==  self.currentUser?.email}
+                            self.userPostsCount.text = "\(currentUserPosts.count) Posts"
+                        }
+                    }
+                    
+                }
+            }
         }
     }
     
--- README.md
+++ README.md
@@ -5,6 +5,7 @@ This app is based on the [IOS20-InstaCloneFirebase](https://github.com/atilsaman
 ## New Features
 
 - Image optimizations before upload to Firebase
+- Profile Page
 - Pull-to-Refresh in feed
 - Bugs and memory leak (in Feed) fixed
 - Layout problems fixed
